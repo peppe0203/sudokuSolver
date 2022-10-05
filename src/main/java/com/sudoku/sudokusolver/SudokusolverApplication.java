fa98cddc08bdc85829f6cec3685357cc2afd3589
Giuseppe Collura <47029198+peppe0203@users.noreply.github.com>
2022-10-05 21:00:46 +0200

    Init commit

---
+++ src/main/java/com/sudoku/sudokusolver/SudokusolverApplication.java
@@ -0,0 +1,13 @@
+package com.sudoku.sudokusolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokusolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokusolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudoku/sudokusolver/SudokusolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoku.sudokusolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokusolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
