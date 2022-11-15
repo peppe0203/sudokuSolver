8e660894e028598d1bc558fe2bf23e059e2b791f
GiuseppeCollura <gcollura@ilionx.com>
2022-11-15 09:35:11 +0100

    another test

--- src/main/java/com/sudoku/sudokusolver/SudokusolverApplication.java
+++ src/main/java/com/sudoku/sudokusolver/SudokusolverApplication.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 @SpringBootApplication
 public class SudokusolverApplication {
 
+	// Testing ssh
 	public static void main(String[] args) {
 		SpringApplication.run(SudokusolverApplication.class, args);
 		Sudoku sudo = new Sudoku();
